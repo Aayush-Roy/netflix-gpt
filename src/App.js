d10d55f6b415e07a17d776c3ef9c6e22e19c60ab
aayush <ar0671362@gmail.com>
2024-12-05 17:54:59 +0530

    signIn/signUp && login/header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,23 @@
 import React from 'react'
-
+import Login from './components/Login'
+import Browse from './components/Browse'
+import { createBrowserRouter } from 'react-router-dom'
+import Body from './components/Body'
+import { RouterProvider } from 'react-router-dom'
 function App() {
+  const appRouter = createBrowserRouter([
+    {
+      path:"/",
+      element:<Login/>,
+    },
+    {
+      path:"/browse",
+      element:<Browse/>,
+    }
+  ])
   return (
-    <div className='bg-red-400'>
-      Hello world
+    <div >
+      <RouterProvider router={appRouter}/>
     </div>
   )
 }
